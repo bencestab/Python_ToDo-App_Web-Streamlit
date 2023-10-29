13ced70529429949aa36d839c006ce58aaa7ea70
bencestab <stabbence@gmail.com>
2023-10-29 17:45:21 +0100

    bug fixed

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-def get_todos(filepath="files/todos.txt"):
+def get_todos(filepath="todos.txt"):
 
     """Read the text file and return the list of to-do items"""
 
@@ -6,7 +6,7 @@ def get_todos(filepath="files/todos.txt"):
             todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="files/todos.txt"):
+def write_todos(todos_arg, filepath="todos.txt"):
 
     """ Write the to-do items in the text file """
     
