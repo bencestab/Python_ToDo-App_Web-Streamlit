5c2f4562418922544981b3e190e039fd047e51c0
bencestab <stabbence@gmail.com>
2023-11-04 21:20:47 +0100

    requirements.txt fixed

--- web_version.py
+++ web_version.py
@@ -3,22 +3,22 @@ import functions
 
 todos = functions.get_todos()
 
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
 
-
 st.title("My ToDo webApp")
 
 for i, todo in enumerate(todos):
-        checkbox = st.checkbox(todo,key=todo)
-        if checkbox:
-              todos.pop(i)
-              functions.write_todos(todos)
-              del st.session_state[todo]
-              st.experimental_rerun()
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(i)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="",
               placeholder="Add a new ToDo...",
